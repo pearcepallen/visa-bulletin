b0dde961d59ae665e665c7ff8fbf78b92a5a2b59
Pearce Allen <pearce.p.allen@gmail.com>
2022-10-18 21:32:26 -0500

    Add Email Model

--- backend/base/models.py
+++ backend/base/models.py
@@ -1,9 +1,20 @@
 from calendar import month
+from email.policy import default
+from enum import unique
+from unittest.util import _MAX_LENGTH
 from django.db import models
 
 class Bulletin(models.Model):
     id = models.AutoField(primary_key=True, editable=False)
     month = models.CharField(max_length=200, null=True, blank=True)
     year = models.CharField(max_length=200, null=True, blank=True)
+    sent = models.BooleanField(default=False)
 
-# Create your models here.
+    class Meta:
+        unique_together = ('month', 'year')
+
+class Email(models.Model):
+    id = models.AutoField(primary_key=True, editable=False)
+    name = models.CharField(max_length=200, null=True, blank=True)
+    email = models.CharField(max_length=200, null=False, unique=True)
+
